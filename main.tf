78b4792f6b9f13ac5454e93bad8212ba9689cc41
Erik Jagyugya <jagyugyaerik@gmail.com>
2022-05-06 22:01:09 +0200

    Change cidr of subnet
    Signed-off-by: Erik Jagyugya <jagyugyaerik@gmail.com>

--- main.tf
+++ main.tf
@@ -5,17 +5,17 @@ resource "google_service_account" "this" {
 
 resource "google_compute_subnetwork" "this" {
   name          = "${var.project_id}-gke"
-  ip_cidr_range = "10.2.0.0/16"
+  ip_cidr_range = "10.5.0.0/20"
   region        = var.region
   network       = google_compute_network.this.id
   secondary_ip_range {
     range_name    = "services-range"
-    ip_cidr_range = "192.168.1.0/24"
+    ip_cidr_range = "10.4.0.0/19"
   }
 
   secondary_ip_range {
     range_name    = "pod-ranges"
-    ip_cidr_range = "192.168.64.0/22"
+    ip_cidr_range = "10.0.0.0/14"
   }
 }
 
