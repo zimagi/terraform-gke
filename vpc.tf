706697be92b55a2fb773f6a09aca5406602fe1b5
jagyugyaerik <jagyugyaerik@gmail.com>
2022-08-17 01:06:23 +0200

    Update peering
    Signed-off-by: jagyugyaerik <jagyugyaerik@gmail.com>

--- vpc.tf
+++ vpc.tf
@@ -33,3 +33,14 @@ module "vpc" {
     ]
   }
 }
+
+resource "google_compute_network_peering_routes_config" "peering_gke_routes" {
+  depends_on = [
+    module.gke
+  ]
+  peering = module.gke.peering_name
+  network = var.vpc_name == "" ? local.vpc_name : var.vpc_name
+
+  import_custom_routes = true
+  export_custom_routes = true
+}
